296822bb67145b29843bcb9ac1ae806362392179
Gaurav Tak <gauravtak996@gmail.com>
2024-04-05 14:57:04 +0530

    changed to deployed api from local

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ const nextConfig = {
 	  //domains: ['storage.googleapis.com']
   },
   env: {
-    ENDPOINT_URL:'http://localhost:4000'
-	  //ENDPOINT_URL:'https://busy-gaiters-toad.cyclic.app'
+    //ENDPOINT_URL:'http://localhost:4000'
+    ENDPOINT_URL:'https://busy-gaiters-toad.cyclic.app'
     // ENDPOINT_URL:'http://34.93.117.21'
   },
 
